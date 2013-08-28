dca6034ff24fd8fb2627d2423276f071baa6744e
Dean Serenevy <dean@serenevy.net>
2013-08-28 19:24:16 -0400

    [...]

--- apci/galil/__init__.py
+++ apci/galil/__init__.py
@@ -6,6 +6,5 @@
 # This software is Copyright (c) 2013 APCI, LLC. All rights reserved.
 from __future__ import division, absolute_import, print_function
 
-from apci.galil.galil import Galil
-from apci.galil.datalog import DataLog, DataLogFileLoader, DataLogCollector, DataLogGalilCollector
-# EVT_DATALOG_BEGIN, EVT_DATALOG_FINISH
+from apci.galil.galil   import Galil
+from apci.galil.datalog import DataLogGalilCollector
